80607204289cdda03a9171c65c56a6e2a81892fa
evenlxz <anthonypearson081@gmail.com>
2024-06-02 13:13:25 -0500

    fuck you tay

---
+++ js/characters.js
@@ -0,0 +1,16 @@
+const characters = [
+    {
+    name: "Albedo",
+    src: "characterIcons/ui-avataricon-albedo.png",
+    element: "Geo",
+    roles: ["DPS", "Off-Field"]
+    }
+]
+
+
+
+
+
+
+
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'electro': '#e600e6',
+        'cryo': '#00ffff',
+        'dendro': '#00ff00',
+        'pyro': '#ff1a1a',
+        'geo': '#ffff00',
+        'anemo': '#99ffcc',
+        'hydro': '#0066ff',
+      }
+    },
+    
   },
   plugins: [],
 }
